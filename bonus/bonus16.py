dc3962b22919524aa3fd977a6b61bcccdb0ff968
tuanv <tuanvunv.bn@gmail.com>
2024-12-17 21:58:54 +0700

    Update bonus16.py

--- bonus/bonus16.py
+++ bonus/bonus16.py
@@ -1,19 +1,28 @@
 import PySimpleGUI as sg
+from zip_create_bonus16 import make_archive
 
 label1 = sg.Text("Select file to compress: ")
 input1 = sg.Input()
-choose_button1 = sg.FilesBrowse("Choose")
+choose_button1 = sg.FilesBrowse("Choose", key="files")
 
 label2 = sg.Text("Select destination folder: ")
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse()
+choose_button2 = sg.FolderBrowse("Browser", key="folder")
 
 compress_button = sg.Button("Compress")
+output = sg.Text(key="output", text_color="green")
 
 window = sg.Window("File Compressor",
                    layout=[[label1, input1, choose_button1],
                            [label2, input2, choose_button2],
-                           [compress_button]])
+                           [compress_button, output]])
 
-window.read()
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths, folder)
+    window["output"].update(value="Compression completed")
+
 window.close()
